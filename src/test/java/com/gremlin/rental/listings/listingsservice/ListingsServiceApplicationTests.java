fc865bb83939af20a0d6c95d568925b0b8b2410f
Mark Franklin <markfranklin@Marks-MacBook-Pro.local>
2019-08-20 15:09:15 -0700

    moved bucket name to env var and updated tests

--- src/test/java/com/gremlin/rental/listings/listingsservice/ListingsServiceApplicationTests.java
+++ src/test/java/com/gremlin/rental/listings/listingsservice/ListingsServiceApplicationTests.java
@@ -1,5 +1,5 @@
 package com.gremlin.rental.listings.listingsservice;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ListingsServiceApplicationTests {
+
+	@BeforeClass
+	public static void setup() {
+		System.setProperty("BUCKET_NAME", "mark.demo");
+	}
 
 	@Test
 	public void contextLoads() {
